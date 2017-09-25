5499ad9bfa15039860edcf76b1bdb23dd1f1363e
shanepeckham <shanepeckham@live.com>
2017-09-25 11:09:08 +0100

    2

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM golang:latest
+
+# Install beego and the bee dev tool
+RUN go get -u -v github.com/astaxie/beego
+RUN go get -u -v github.com/beego/bee
+RUN go get -u -v github.com/Microsoft/ApplicationInsights-Go/appinsights
+RUN go get -u -v gopkg.in/mgo.v2
+RUN go get -u -v github.com/Azure/go-autorest/autorest/utils
+
+ENV GOPATH /go
+ENV PATH $GOPATH/bin:$PATH
+ENV DATABASE=
+ENV PASSWORD=
+ENV INSIGHTSKEY=
+ENV SOURCE=
+ENV EVENTURL=
+ENV EVENTPOLICYNAME=
+ENV EVENTPOLICYKEY=
+
+# Copy the application files (needed for production)
+ADD . /go/src/captureorderfd
+
+# Set the working directory to the app directory
+WORKDIR /go/src/captureorderfd
+
+# Expose the application on port 8080
+EXPOSE 8080
+
+# Set the entry point of the container to the bee command that runs the
+# application and watches for changes
+CMD ["bee", "run"]
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,108 +7,12 @@ import (
 
 func init() {
 
-	beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
-			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method: "Get",
-			Router: `/:objectId`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:objectId`,
-			AllowHTTPMethods: []string{"put"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:objectId`,
-			AllowHTTPMethods: []string{"delete"},
-			MethodParams: param.Make(),
-			Params: nil})
-
 	beego.GlobalControllerRouter["captureorderfd/controllers:OrderController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:OrderController"],
 		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
+			Method:           "Post",
+			Router:           `/`,
 			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:UserController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:UserController"],
-		beego.ControllerComments{
-			Method: "Post",
-			Router: `/`,
-			AllowHTTPMethods: []string{"post"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:UserController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:UserController"],
-		beego.ControllerComments{
-			Method: "GetAll",
-			Router: `/`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:UserController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:UserController"],
-		beego.ControllerComments{
-			Method: "Get",
-			Router: `/:uid`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:UserController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:UserController"],
-		beego.ControllerComments{
-			Method: "Put",
-			Router: `/:uid`,
-			AllowHTTPMethods: []string{"put"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:UserController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:UserController"],
-		beego.ControllerComments{
-			Method: "Delete",
-			Router: `/:uid`,
-			AllowHTTPMethods: []string{"delete"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:UserController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:UserController"],
-		beego.ControllerComments{
-			Method: "Login",
-			Router: `/login`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
-
-	beego.GlobalControllerRouter["captureorderfd/controllers:UserController"] = append(beego.GlobalControllerRouter["captureorderfd/controllers:UserController"],
-		beego.ControllerComments{
-			Method: "Logout",
-			Router: `/logout`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
+			MethodParams:     param.Make(),
+			Params:           nil})
 
 }
