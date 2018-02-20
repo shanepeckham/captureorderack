1dafa51c61bf6bc6223c01d0f04e71cddd9da169
shanepeckham <shanepeckham@live.com>
2018-02-20 12:42:30 +0000

    AMQP and Optimisations

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
-docker build -t shanepeckham/captureorderack:v5 . 
-docker push shanepeckham/captureorderack:v5
+docker build -t shanepeckham/captureorderack:amqp . 
+docker push shanepeckham/captureorderack:amqp
